58517f7781dafe899af3a893dfe5869262b11895
Bimesh De Silva <bimeshde@gmail.com>
2019-06-03 13:28:01 -0400

    Calling crypto exchange api

--- server/exchange/go.mod
+++ server/exchange/go.mod
@@ -3,7 +3,8 @@ module github.com/Bimde/crypto-trader/exchange
 go 1.12
 
 require (
-	github.com/Bimde/crypto-trader/build v0.0.0-20190603000605-f0f546ec75e3
-	github.com/golang/protobuf v1.3.1
+	github.com/Bimde/crypto-trader/build v0.0.0-20190603135557-f1848a70a1e7
+	github.com/Bimde/httputils v0.0.0-20190413154510-a18d71744d12
+	github.com/sirupsen/logrus v1.4.2 // indirect
 	google.golang.org/grpc v1.21.0
 )
--- server/exchange/main.go
+++ server/exchange/main.go
@@ -4,14 +4,20 @@ import (
 	"context"
 	"log"
 	"net"
+	"fmt"
 
 	"github.com/Bimde/crypto-trader/build"
+	"github.com/Bimde/httputils"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
 
 type server struct{}
+
+var http httputils.Client
+
+const api = "https://api.cryptonator.com/api/ticker/%s"
 
 func main() {
 	lis, err := net.Listen("tcp", ":7070")
@@ -22,11 +28,21 @@ func main() {
 	s := grpc.NewServer()
 	build.RegisterStockExchangeServer(s, &server{})
 	reflection.Register(s)
+	http = httputils.Client{}
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve: %v", err)
 	}
 }
 
 func (s *server) GetStockPrice(c context.Context, req *build.GetStockPriceRequest) (*build.GetStockPriceResponse, error) {
-	return &build.GetStockPriceResponse{Stock: []*build.Stock{&build.Stock{Ticker: "GOOG", Price: 21.22}}}, nil
+	var response build.CryptonatorResponse
+	http.Get(nil, getAPIURL(req.Ticker[0]), nil, response)
+	if response.Ticker != nil {
+		return &build.GetStockPriceResponse{Stock: []*build.Stock{&build.Stock{Ticker: response.Ticker.Base, Price: response.Ticker.Price}}}, nil
+	}
+	return nil, fmt.Errorf("Error getting quote for ticker %s", req.Ticker[0])
+}
+
+func getAPIURL(ticker string) string {
+	return fmt.Sprintf(api, ticker)
 }
